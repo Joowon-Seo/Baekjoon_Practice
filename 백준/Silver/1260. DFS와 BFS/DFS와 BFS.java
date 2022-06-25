16704586e84dafaefa6face28e7f41fb6d9d2316
Joowon-Seo <90291431+Joowon-Seo@users.noreply.github.com>
2022-06-26 04:08:33 +0900

    [Silver II] Title: DFS와 BFS, Time: 364 ms, Memory: 24500 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -2,16 +2,45 @@ import java.io.*;
 import java.util.*;
 public class Main {
 
-    static int[][] map;
-    static boolean[] visit;
     static int n, m, v;
+    static int[][] map;
+    static boolean[] visited;
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        v = Integer.parseInt(st.nextToken());
+
+        map = new int[n+1][n+1];
+        visited = new boolean[n+1];
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+
+            map[x][y] = 1;
+            map[y][x] = 1;
+        }
+
+        dfs(v);
+        System.out.println();
+        visited = new boolean[n + 1];
+        bfs(v);
+
+
+    }
 
     public static void dfs(int i){
-        visit[i] = true;
+        visited[i] = true;
         System.out.print(i + " ");
 
-        for (int j = 1; j <n+1; j++) {
-            if (map[i][j] == 1 && visit[j] == false){
+        for (int j = 1; j < n+1; j++) {
+            if (map[i][j] == 1 && !visited[j]){
                 dfs(j);
             }
         }
@@ -19,52 +48,26 @@ public class Main {
 
     public static void bfs(int i){
         Queue<Integer> queue = new LinkedList<>();
-        queue.offer(i);
-        visit[i] = true;
+        visited[i] = true;
+        queue.add(i);
 
         while (!queue.isEmpty()){
-            int tmp = queue.poll();
-            System.out.print(tmp + " ");
+            int cur = queue.poll();
+            System.out.print(cur + " ");
 
             for (int j = 1; j < n + 1; j++) {
-                if (map[tmp][j] == 1 && visit[j] == false){
-                    queue.offer(j);
-                    visit[j] = true;
+                if (map[cur][j] == 1 && !visited[j]){
+                    queue.add(j);
+                    visited[j] = true;
                 }
+
             }
+
         }
     }
 
 
 
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine()," ");
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
-        v = Integer.parseInt(st.nextToken());
-
-        map = new int[n+1][n+1];
-        visit = new boolean[n+1];
-        for (int i = 0; i < m; i++) {
-            st = new StringTokenizer(br.readLine(), " ");
-            int x = Integer.parseInt(st.nextToken());
-            int y = Integer.parseInt(st.nextToken());
-            map[x][y] = 1;
-            map[y][x] = 1;
-
-        }
-
-        dfs(v);
-        System.out.println();
-        Arrays.fill(visit, false);
-        bfs(v);
-
-
-
-
-    }
 }
 
 
