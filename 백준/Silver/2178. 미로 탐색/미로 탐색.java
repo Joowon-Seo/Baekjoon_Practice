171b2751392d79ee0ec87de68015ad7d55b4b986
Joowon-Seo <90291431+Joowon-Seo@users.noreply.github.com>
2022-06-26 04:27:17 +0900

    [Silver I] Title: 미로 탐색, Time: 132 ms, Memory: 14648 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -2,38 +2,42 @@ import java.io.*;
 import java.util.*;
 public class Main {
 
+    static int n, m;
     static int[][] map;
     static boolean[][] visited;
-    static int N, M;
-    static int [] dx = {-1, 1, 0, 0};// 상하
-    static int [] dy = {0, 0, -1, 1};// 좌우
+    static int[] dX = {-1, 1, 0, 0};
+    static int[] dY = {0, 0, -1, 1};
 
 
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
 
-        visited = new boolean[N][M];
-        map = new int[N][M];
-        for (int i = 0; i < N; i++) {
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        map = new int[n][m];
+        visited = new boolean[n][m];
+
+        for (int i = 0; i < n; i++) {
             String str = br.readLine();
-            for (int j = 0; j < M; j++) {
+            for (int j = 0; j < m; j++) {
                 map[i][j] = str.charAt(j) - '0';
             }
         }
 
         visited[0][0] = true;
         bfs(0,0);
-        System.out.println(map[N-1][M-1]);
+
+        System.out.println(map[n-1][m-1]);
 
 
 
     }
 
     public static void bfs(int x, int y){
+
         Queue<int[]> queue = new LinkedList<>();
         queue.add(new int[] {x, y});
 
@@ -44,26 +48,30 @@ public class Main {
             int curY = cur[1];
 
             for (int i = 0; i < 4; i++) {
-                int nextX = curX + dx[i];
-                int nextY = curY + dy[i];
+                int nextX = curX + dX[i];
+                int nextY = curY + dY[i];
 
-                if (nextX < 0 || nextY < 0 || nextX >= N || nextY >= M){
+                if (nextX < 0 || nextY < 0 || nextX >= n || nextY >= m){
                     continue;
                 }
 
-                if (visited[nextX][nextY] || map[nextX][nextY] == 0){
+                if (map[nextX][nextY] == 0 || visited[nextX][nextY]){
                     continue;
                 }
 
                 queue.add(new int[] {nextX, nextY});
-                map[nextX][nextY] = map[curX][curY] + 1;
                 visited[nextX][nextY] = true;
+                map[nextX][nextY] = map[curX][curY] + 1;
 
             }
 
-
         }
+
+
     }
+
+
+
 }
 
 
