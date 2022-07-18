ab2e6380366c0b56c67a21bce289194d816799d1
Joowon-Seo <90291431+Joowon-Seo@users.noreply.github.com>
2022-07-18 23:53:18 +0900

    [Silver II] Title: 주식, Time: 1128 ms, Memory: 317424 KB -BaekjoonHub

--- 백준/Silver/11501. 주식/주식.java
+++ 백준/Silver/11501. 주식/주식.java
@@ -1,27 +1,72 @@
+import java.io.*;
 import java.util.*;
 
-public class Main {		
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int testcase = sc.nextInt();
-		long ans[] = new long[testcase];
-		for( int n = 0 ; n < testcase ; n++ ) {
-			int days = sc.nextInt();
-			long stocks[] = new long[days];
-			long max = 0;
-			for( int i = 0 ; i < days ; i++ ) {
-				stocks[i] = sc.nextInt();
-			}
-			for( int i = days-1 ; i >= 0 ; i-- ) {
-				if(stocks[i] > max) {
-					max = stocks[i];
-				}else {
-					ans[n] += (max - stocks[i]);
-				}
-			}		
-		}
-		for( long i : ans) {
-			System.out.println(i);
-		}
-	}
+
+public class Main {
+
+    public static int T;
+    public static int N;
+    public static int[] price;
+    public static long gain;  // 총 이득
+    public static int max; // 현재까지 가장 비싼 가격
+
+
+
+
+   public static void main(String[] args) throws IOException {
+
+       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+       BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+//       StringTokenizer st = new StringTokenizer(br.readLine());
+
+       T = Integer.parseInt(br.readLine());
+
+       for (int i = 0; i < T; i++) {
+
+           N = Integer.parseInt(br.readLine());
+           price = new int[N];
+           gain = 0;
+           max = 0;
+           StringTokenizer st = new StringTokenizer(br.readLine());
+           for (int j = 0; j < N; j++) {
+               price[j] = Integer.parseInt(st.nextToken());
+           }
+
+
+
+           for (int j = N - 1; j >= 0; j--) {
+               if (price[j] > max){
+                   max = price[j];
+               } else {
+                   gain += max - price[j];
+               }
+           }
+
+            bw.write(gain + "\n");
+
+       }
+
+       br.close();
+       bw.close();
+
+
+    }
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
